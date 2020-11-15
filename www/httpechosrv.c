63ec8c309fbe9382ddabab3c1a61b107bd258df1
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2020-11-15 16:26:33 -0700

    i hate threads

--- www/httpechosrv.c
+++ www/httpechosrv.c
@@ -13,6 +13,10 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
+//from PA3 operating systems
+#define UTIL_FAILURE -1
+#define UTIL_SUCCESS 0
+
 #define MAXLINE  8192  /* max text line length */
 #define MAXBUF   8192  /* max I/O buffer size */
 #define LISTENQ  1024  /* second argument to listen() */
@@ -20,7 +24,12 @@
 int open_listenfd(int port);
 void *thread(void *vargp);
 char* itoa(int value, char* result, int base);
-int dnslookup(const char* hostname, char* firstIPstr, int maxSize);
+int hostname_to_ip(char* hostname , char* ip);
+
+struct Thread_object{
+    int* connfdp;
+    char* ip_addr;
+};
 
 int main(int argc, char **argv) 
 {
@@ -37,23 +46,36 @@ int main(int argc, char **argv)
     listenfd = open_listenfd(port);
     printf("listening on port %d\n", port);
 
+
     while (1) {    
+    //prepare thread object to be sent to thread
+    struct Thread_object thread_obj;
+    thread_obj.ip_addr = (char *)malloc(sizeof(char) *100);
+    thread_obj.connfdp = malloc(sizeof(int));
+    thread_obj.connfdp = accept(listenfd, (struct sockaddr*)&clientaddr, &clientlen);
+
+    pthread_create(&tid, NULL, thread, (void *)&thread_obj);
+    }
+   /* while (1) {    
 	connfdp = malloc(sizeof(int));
 	*connfdp = accept(listenfd, (struct sockaddr*)&clientaddr, &clientlen);
 	pthread_create(&tid, NULL, thread, connfdp);
-    }
+    }*/
 }
 
 /* thread routine */
 void * thread(void * vargp) 
 {  
-  /*  char cwd[MAXBUF];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       printf("Current working dir: %s\n", cwd);
-   }*/
 
-    int connfd = *((int *)vargp);
-    free(vargp);
+    struct Thread_object *thread_object;
+    thread_object = vargp;
+
+
+    //int connfd = *((int *)vargp);
+    //free(vargp);
+    
+    int connfd = thread_object->connfdp;
+
     pthread_detach(pthread_self()); 
     size_t n;
   
@@ -91,13 +113,11 @@ void * thread(void * vargp)
         domain_name = (char *)malloc(sizeof(char) *100);
         domain_name = strtok(NULL, " ");
 
-        char resolved_domain;
+        printf("%s\n",domain_name );
+        
+        char* resolved_domain;
         resolved_domain = (char *)malloc(sizeof(char) *100); 
-
-        dnslookup(domain_name, resolved_domain, 100);
-
-        printf("%s\n",resolved_domain );
-
+        hostname_to_ip(&domain_name, &resolved_domain);
     }//nested if
 
     //not a GET request, bye
@@ -158,7 +178,7 @@ int open_listenfd(int port)
 } /* end open_listenfd */
 
 
-int socket_connect(char *host, in_port_t port){
+/*int socket_connect(char *host, in_port_t port){
     struct hostent *hp;
     struct sockaddr_in addr;
     int on = 1, sock;     
@@ -184,7 +204,7 @@ int socket_connect(char *host, in_port_t port){
 
     }
     return sock;
-}
+}*/
 
 //function to convert into to string
 //source:https://stackoverflow.com/questions/8257714/how-to-convert-an-int-to-string-in-c/23840699#:~:text=You%20can%20use%20itoa(),to%20convert%20any%20value%20beforehand.
@@ -217,73 +237,28 @@ char* itoa(int value, char* result, int base) {
     return result;
 }
 
-//from operating systems PA3
-int dnslookup(const char* hostname, char* firstIPstr, int maxSize){
-
-    /* Local vars */
-    struct addrinfo* headresult = NULL;
-    struct addrinfo* result = NULL;
-    struct sockaddr_in* ipv4sock = NULL;
-    struct in_addr* ipv4addr = NULL;
-    char ipv4str[INET_ADDRSTRLEN];
-    char ipstr[INET6_ADDRSTRLEN];
-    int addrError = 0;
-
-    /* DEBUG: Print Hostname*/
-#ifdef UTIL_DEBUG
-    fprintf(stderr, "%s\n", hostname);
-#endif
-   
-    /* Lookup Hostname */
-    addrError = getaddrinfo(hostname, NULL, NULL, &headresult);
-    if(addrError){
-    fprintf(stderr, "Error looking up Address: %s\n",
-        gai_strerror(addrError));
-    return UTIL_FAILURE;
-    }
-    /* Loop Through result Linked List */
-    for(result=headresult; result != NULL; result = result->ai_next){
-    /* Extract IP Address and Convert to String */
-    if(result->ai_addr->sa_family == AF_INET){
-        /* IPv4 Address Handling */
-        ipv4sock = (struct sockaddr_in*)(result->ai_addr);
-        ipv4addr = &(ipv4sock->sin_addr);
-        if(!inet_ntop(result->ai_family, ipv4addr,
-              ipv4str, sizeof(ipv4str))){
-        perror("Error Converting IP to String");
-        return UTIL_FAILURE;
-        }
-#ifdef UTIL_DEBUG
-        fprintf(stdout, "%s\n", ipv4str);
-#endif
-        strncpy(ipstr, ipv4str, sizeof(ipstr));
-        ipstr[sizeof(ipstr)-1] = '\0';
-    }
-    else if(result->ai_addr->sa_family == AF_INET6){
-        /* IPv6 Handling */
-#ifdef UTIL_DEBUG
-        fprintf(stdout, "IPv6 Address: Not Handled\n");
-#endif
-        strncpy(ipstr, "UNHANDELED", sizeof(ipstr));
-        ipstr[sizeof(ipstr)-1] = '\0';
-    }
-    else{
-        /* Unhandlded Protocol Handling */
-#ifdef UTIL_DEBUG
-        fprintf(stdout, "Unknown Protocol: Not Handled\n");
-#endif
-        strncpy(ipstr, "UNHANDELED", sizeof(ipstr));
-        ipstr[sizeof(ipstr)-1] = '\0';
-    }
-    /* Save First IP Address */
-    if(result==headresult){
-        strncpy(firstIPstr, ipstr, maxSize);
-        firstIPstr[maxSize-1] = '\0';
-    }
+//https://www.binarytides.com/hostname-to-ip-address-c-sockets-linux/
+int hostname_to_ip(char * hostname , char* ip)
+{
+    struct hostent *he;
+    struct in_addr **addr_list;
+    int i;
+        
+    if ( (he = gethostbyname( hostname ) ) == NULL) 
+    {
+        // get the host info
+        herror("gethostbyname");
+        return 1;
     }
 
-    /* Cleanup */
-    freeaddrinfo(headresult);
-
-    return UTIL_SUCCESS;
-}//dns lookup
+    addr_list = (struct in_addr **) he->h_addr_list;
+    
+    for(i = 0; addr_list[i] != NULL; i++) 
+    {
+        //Return the first one;
+        strcpy(ip , inet_ntoa(*addr_list[i]) );
+        return 0;
+    }
+    
+    return 1;
+}
