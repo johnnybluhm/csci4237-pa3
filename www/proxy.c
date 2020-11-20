3760342b3d50d99f2fbd70614785cbe9a1ee2179
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2020-11-20 14:51:28 -0700

    need to handle binary data

--- www/proxy.c
+++ www/proxy.c
@@ -98,7 +98,8 @@ void * thread(void * vargp)
 
     //various string vars needed. All declared with malloc so they go on heap. 
     char* resolved_name = malloc(sizeof(char)*MAXBUF);  
-    char* domain_name = malloc(sizeof(char)*MAXBUF);  
+    char* url_raw = malloc(sizeof(char)*MAXBUF);  
+    char* save_ptr = malloc(sizeof(char)*MAXBUF); 
     char* request = malloc(sizeof(char)*MAXBUF);
     char* request_type = malloc(sizeof(char)*MAXBUF);
     char* http_packet = malloc(sizeof(char) *MAXBUF);
@@ -119,24 +120,30 @@ void * thread(void * vargp)
     }
 
     sscanf(request, "%[^\n]", request_header);
-    printf("%s\n",request_header);
     sscanf(request, "%s", request_type);
 
+    memset(http_packet, 0, strlen(http_packet)); 
     strcpy(http_packet,request);
    
     //nested if to check if GET request 
-    if(strcmp(request_type, "GET") == 0){
+    if(strcmp(request_type, "GET") == 0){              
         
+        //got to use twice to get second element seperated by space
+        strtok_r(request," ",save_ptr);
+        url_raw = strtok_r(NULL," ",save_ptr);
+
         //https://stackoverflow.com/questions/726122/best-ways-of-parsing-a-url-using-c
-        // Parsing the tmp_source char*
-        if (sscanf(request, "http://%99[^:]:%i/%199[^\n]", ip, &port, page) == 3) { succ_parsing = 1;}
-        else if (sscanf(request, "http://%99[^/]/%199[^\n]", ip, page) == 2) { succ_parsing = 1;}
-        else if (sscanf(request, "http://%99[^:]:%i[^\n]", ip, &port) == 2) { succ_parsing = 1;}
-        else if (sscanf(request, "http://%99[^\n]", ip) == 1) { succ_parsing = 1;}
+        //if (sscanf(url_raw, "http://%99[^:]:%i/%199[^\n]", ip, &port, page) == 3) { succ_parsing = 1;}
+        if (sscanf(url_raw, "http://%99[^/]/%199[^\n]", ip, page) == 2) { succ_parsing = 1;}
+        else if (sscanf(url_raw, "http://%99[^:]:%i[^\n]", ip, &port) == 2) { succ_parsing = 1;}
+        else if (sscanf(url_raw, "http://%99[^/]", ip) == 1) { succ_parsing = 1;}
+        else{ printf("Error parsing URL\n"); return NULL;}
+
+
         //sscanf(request_header, "http://%99%", domain_name);
-        printf("domain name is %s port is %d page is %s \n", ip, port, page );
-        sscanf(request_header, "%*[^/]%*[/]%[^/]", host_name);
-        printf("hostbamne is %s\n",host_name );
+        //printf("domain name is %s port is %d page is %s \n", ip, port, page );
+        //sscanf(request_header, "%*[^/]%*[/]%[^/]", host_name);
+        //printf("hostname is %s\n",host_name );
 
         //---- ---CRITICAL SECTION------ lock is good in if and else
         pthread_mutex_lock(thread_object->file_lock);
@@ -146,13 +153,14 @@ void * thread(void * vargp)
         if( fp == NULL){
             printf("File not found\n");
 
-            int dns_ret = hostname_to_ip(host_name, resolved_name);
+            int dns_ret = hostname_to_ip(ip, resolved_name);
 
-            printf("%s\n",resolved_name);
+            printf("resolved name is %s\n",resolved_name);
+
             if(dns_ret == 0 ){
                 printf("Saving address to file\n");
             
-                fp = fopen(host_name, "w");
+                fp = fopen(ip, "w");
 
                 fputs(resolved_name, fp);
                 fputs("\n", fp);
@@ -177,6 +185,7 @@ void * thread(void * vargp)
         int check_addr; 
 
         //takes pointer to sockaddr_in and ip_addr as string
+        printf("resolved_name is %s\n", resolved_name);
         check_addr = build_server_addr(serv_addr, resolved_name);
 
         if(check_addr < 0){
@@ -195,42 +204,38 @@ void * thread(void * vargp)
                     return NULL; 
                 }
             int n;
-            n = write(sock, http_packet, sizeof(char)*strlen(http_packet));
+
+            printf("HTTP PACKET IS \n%s\n",http_packet );
+            n = write(sock, http_packet, strlen(http_packet));
             if(n<0){
                 printf("Error writing\n");
                 return NULL;
             }
-            printf("wrote %d bytes\n", n);
+            printf("wrote %d bytes to server\n", n);
 
             while(1){
             int bytes_read;
-            memset(http_response, 0, sizeof(char)*strlen(http_response)); 
-            bytes_read = read(sock, http_response, sizeof(char)*MAXBUF);
-           /* while(*ret = read(sock, http_response, sizeof(char)*MAXBUF) >0){
-                printf("read %d bytes\n",*ret);
-                *i = *i + 1;
-                printf("value of i is %d\n",*i);
-                printf("RESPONSE IS:\n%s\n",http_response);
-                strcpy(http_response,http_response_copy);
-                bzero(http_response, sizeof(char)*MAXBUF);
-            }//while*/
+            memset(http_response, 0, MAXBUF); 
+            bytes_read = read(sock, http_response, MAXBUF);
+            printf("HTTP RESPONSE:\n%s\n",http_response);
             if(bytes_read < 0){
                 printf("Error reading from network socket.\n");
                 return NULL;
             }
-            printf("read %d bytes\n",bytes_read);
+            printf("read %d bytes from server\n",bytes_read);
             int m;
             if(bytes_read> 0){
-                m = write(connfd, http_response, sizeof(char)*strlen(http_response));
-                if(m < 0){
+                m = write(thread_object->connfdp, http_response, strlen(http_response));
+                /*if(m < 0){
                     printf("Error writing back to client\n");
                     return NULL;
-                }
-                printf("wrote %d bytes\n",m);
+                }*/
+                printf("wrote %d bytes back to client\n",m);
             }
 
-            if(bytes_read == 0){
+            if(bytes_read == 0){                
                 //exit loop
+                //close(sock);
                 break;
             }
         }//forever while
@@ -280,6 +285,7 @@ int build_server_addr(struct sockaddr_in* serv_addr, char * ip_add){
     if(inet_aton(ip_add, (struct in_addr* )&serv_addr->sin_addr.s_addr) ==0){
         return -1;
     }
+    printf("Address built successfully\n");
     return 1;
 }
 
